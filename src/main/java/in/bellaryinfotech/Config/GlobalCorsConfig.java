385c3db70fca3e1ce74462e8619655053b547ec0
Bellaryinfotech <bellaryinfotechsolutions@gmail.com>
2025-10-25 16:27:59 +0530

    commit ont he global config

--- src/main/java/in/bellaryinfotech/Config/GlobalCorsConfig.java
+++ src/main/java/in/bellaryinfotech/Config/GlobalCorsConfig.java
@@ -1,7 +1,10 @@
 package in.bellaryinfotech.Config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +13,14 @@ public class GlobalCorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+    	
+    	 CorsConfiguration config = new CorsConfiguration();
+
+         // ✅ Allow all required frontend domains
+         config.setAllowedOriginPatterns(Arrays.asList(
+             "http://localhost:5173",    
+             "https://lre.bellaryinfotech.com"
+         ));
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
