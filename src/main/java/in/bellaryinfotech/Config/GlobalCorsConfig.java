aafd8edaee383fadaa52eec626aa28d4b17e32a3
Bellaryinfotech <bellaryinfotechsolutions@gmail.com>
2025-10-25 18:11:10 +0530

    commit in app

--- src/main/java/in/bellaryinfotech/Config/GlobalCorsConfig.java
+++ src/main/java/in/bellaryinfotech/Config/GlobalCorsConfig.java
@@ -20,7 +20,8 @@ public class GlobalCorsConfig {
          config.setAllowedOriginPatterns(Arrays.asList(
              "http://localhost:5173",    
              "http://lre.bellaryinfotech.com",
-             "http://195.35.45.56:5170"
+             "http://195.35.45.56:5170",
+             "http://localrealestate.bellaryinfotech.com"
          ));
         return new WebMvcConfigurer() {
             @Override
